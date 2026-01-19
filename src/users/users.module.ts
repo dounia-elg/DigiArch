1fd0a0db67d332640d8582d8839d86a8047237f0
dounia-elg <dounia.elg20@gmail.com>
2026-01-19 14:37:11 +0100

    feat: DIG-15 update UsersModule to include Mongoose integration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User, UserSchema } from './user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  providers: [UsersService]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
